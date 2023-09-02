76184fa3f09de2642b6b03bdd851b540e7a43500
Theodore GNIMAVO <theodore.gnimavo@eleves.enpc.fr>
2023-09-02 11:48:22 +0200

    password_disance added

--- services/password_distance.py
+++ services/password_distance.py
@@ -1,6 +1,22 @@
 
 def compute_levenshtein_distance(string1, string2):
-    pass
+    if len(string1) == 0:
+        return len(string2)
+    elif len(string2) == 0:
+        return len(string1)
+    elif string1[0] == string2[0]:
+        return compute_levenshtein_distance(tail(string1), tail(string2)
+                                            )
+    else:
+        value_a = compute_levenshtein_distance(tail(string1), string2)
+        value_b = compute_levenshtein_distance(string1, tail(string2))
+        value_c = compute_levenshtein_distance(tail(string1),
+                                               tail(string2))
+        return 1 + min(value_a, value_b, value_c)
+
+
+def tail(string):
+    return string[1:]
 
 
 if __name__ == "__main__":
